63758957a5d2d78a71787ec853f2ec7acb0608a4
Wyatt Carbonell <wyattcarbonell@gmail.com>
2023-03-17 17:27:06 -0400

    Successful implementation of ABvacmetric0 with additional Mathematica code for testing

--- ABvacmetric0.py
+++ ABvacmetric0.py
@@ -1,5 +1,9 @@
-from scipy.integrate import solve_ivp
+import math
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+
 
 def vectorfield(t, w, p):
     
@@ -21,15 +25,16 @@ def vectorfield(t, w, p):
     # create f = [A', B', C', D']
     f = [C,
          D,
-         ]
-         # waiting to check math before finishing implementing the equations
+         -(O_r / (2 * np.exp(4 * A))) + ((3 / 2) * O_L) + (O_k / (2 * np.exp(2 * (A - B)))) - (O_B / (2 * np.exp(4 * (A - B)))) - ((3 / 2) * math.pow(C, 2)) - ((3 / 2) * math.pow(D, 2)),
+         -(3 * C * D) - (O_k / (np.exp(2 * (A - B)))) - (2 * O_B / (np.exp(4 * (A - B))))]
+         
     
     return f
 
-def hitZMax(t, w):
+def hitZMax(t, w, p):
     
     """
-    Checks if we hit zmax in all directions. Acts as helper function to solve_ivp call in ABvacmetric0 to act as an event trigger.
+    Checks if we hit zmax in any direction. Acts as helper function to solve_ivp call in ABvacmetric0 to act as an event trigger.
     """
     
     # set the largest value of redshift to integrate to
@@ -39,9 +44,10 @@ def hitZMax(t, w):
     A = w[0]
     B = w[1]
     
-    # need some condition in the form of a continuous function to check zmax. It will trigger on the condition hitting 0.
+    # we want to stop when either direction hits zmax, so that our domain is valid everywhere
+    return min(np.exp(-(A + 2 * B)) - 1 - (zmax + .1), np.exp(-(A - B)) - 1 - (zmax + .1))
 
-def contracting(t, w):
+def contracting(t, w, p):
     
     """
     Checks for contracting universes. Acts as helper function to solve_ivp call in ABvacmetric0 to act as an event trigger.
@@ -52,7 +58,7 @@ def contracting(t, w):
     D = w[3]
     
     # stop integration if we hit this condition equal to 0
-    return C(t) + 2*D(t)
+    return C + 2*D
     
     
 def ABvacmetric0(p):
@@ -69,28 +75,70 @@ def ABvacmetric0(p):
     B0 = p[5]
     
     #Designate events to check either as terminal or directional via monkey patch.
-    contracting.terminate = True;
-    hitZMax.terminate = True;
+    contracting.terminal = True;
+    hitZMax.terminal = True;
     
     # solve_ivp parameters
     continuousOutput = True; # whether or not to output a continuous solution
     vectorizedInput = True; # defines whether or not the solver is handling a system or an individual ODE
-    tRange = [np.NINF, 0] # defines the range of t values to solve over
+    tRange = [0, np.NINF] # defines the range of t values to solve over
     initVal = [0, 0, 1, B0] # defines initial values for A, B, C, and D
     eventsToCheck = [contracting, hitZMax] # defines the functions to check for termination of integration. Must be of form event([A, B, C, D], t). Event triggers on event() = 0.
     
     # calculate the solution. Returns a Bunch object with many fields. See documentation: https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.solve_ivp.html#scipy.integrate.solve_ivp
-    solution = solve_ivp(vectorfield, tRange, initVal, dense_output = continuousOutput, vectorized = vectorizedInput, events = eventsToCheck, args = (p))
+    solution = solve_ivp(vectorfield, tRange, initVal, dense_output = continuousOutput, vectorized = vectorizedInput, events = eventsToCheck, args = (p,))
     
     sol = solution.sol # the continuous solution of the differential equation as instance of OdeSolution
     t_events = solution.t_events # tracks the occurences of event triggers during integration
+    times = solution.t # the time points that were considered
     
     # if there was an event that triggered from contracting, return the solution and the 'contract flag' set to True
-    if(t_events[0].length > 0):
-        return [sol, True]
+    if(t_events[0].size > 0):
+        return [sol, True, times]
     
-    # if we got ehre, there was no contraction, and we can return a False 'contract flag'
-    return [sol, False]
+    # if we got here, there was no contraction, and we can return a False 'contract flag'
+    return [sol, False, times]
+
+# this test main function calls ABvacmetric0 for a given set of parameters and graphs the resulting A and B functions
+def main():
+    
+    p = [.28, .01, .69, .01, .01, 0]
+    
+    solution = ABvacmetric0(p)
+    
+    equation = solution[0]
+    contract = solution[1]
+    
+    tmin = equation.t_min
+    tmax = equation.t_max
+    
+    print(tmax, tmin)
+    
+    
+    tpoints = range(int(tmin * 1000), int(tmax * 1000))
+    actualtpoints = []
+    
+    for i in range(len(tpoints)):
+        actualtpoints.append(tpoints[i] / 1000)
+        
+    ypointsA = []
+    ypointsB = []
+
+    
+    for i in actualtpoints:
+        point = equation(i)
+        A = point[0]
+        B = point[1]
+        ypointsA.append(A)
+        ypointsB.append(B)
+        
+    plt.plot(actualtpoints, ypointsA, label = "A")
+    plt.plot(actualtpoints, ypointsB, label = "B")
+    plt.legend()
+    plt.show()
+
+    
+main()
     
     
     

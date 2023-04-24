f3145461ebdbc0bffdf150859b4c4f9652f9f78b
Wyatt Carbonell <wyattcarbonell@gmail.com>
2023-04-24 11:38:12 -0400

    fixed the monte carlo

--- MonteCarloSimulation.py
+++ MonteCarloSimulation.py
@@ -8,7 +8,8 @@ def MCMC(dataset, totalPoints, initPoint, run = 0):
     
     initChi2 = chiSquared(initPoint, dataset)
     
-    stepSize = .000001
+    stepSize = .00001
+    angleStepSize = .001
     
     currentPoint = initPoint
     currentChi2 = initChi2
@@ -29,8 +30,8 @@ def MCMC(dataset, totalPoints, initPoint, run = 0):
         
         numberStepsIn100 = 0;
         
-        for i in range(100):
-            candPoint = currentPoint
+        for j in range(100):
+            candPoint = currentPoint.copy()
             
             candPoint[0] = candPoint[0] + ((2 * stepSize * np.random.rand(1)[0]) - (1 * stepSize))
             candPoint[1] = candPoint[1] + ((2 * stepSize * np.random.rand(1)[0]) - (1 * stepSize))
@@ -51,18 +52,20 @@ def MCMC(dataset, totalPoints, initPoint, run = 0):
             # Set O_k to fit the other parameters
             candPoint[3] = 1 - candPoint[0] - candPoint[1] - candPoint[2] - candPoint[4] - np.power(candPoint[5], 2)
             
-            currentDir = candPoint[7]
+            currentDir = candPoint[7].copy()
             n0vec = [0, 0, 0]
-            n0vec[0] = (currentDir[0] + ((2 * stepSize * np.random.rand(1)[0]) - (1 * stepSize)))
-            n0vec[1] = (currentDir[1] + ((2 * stepSize * np.random.rand(1)[0]) - (1 * stepSize)))
-            n0vec[2] = (currentDir[2] + ((2 * stepSize * np.random.rand(1)[0]) - (1 * stepSize)))
-            n0vec[0] = (currentDir[0] / np.linalg.norm(np.array(currentDir)))
-            n0vec[1] = (currentDir[1] / np.linalg.norm(np.array(currentDir)))
-            n0vec[2] = (currentDir[2] / np.linalg.norm(np.array(currentDir)))
+            n0vec[0] = (currentDir[0] + ((2 * angleStepSize * np.random.rand(1)[0]) - (1 * angleStepSize)))
+            n0vec[1] = (currentDir[1] + ((2 * angleStepSize * np.random.rand(1)[0]) - (1 * angleStepSize)))
+            n0vec[2] = (currentDir[2] + ((2 * angleStepSize * np.random.rand(1)[0]) - (1 * angleStepSize)))
+            n0vec[0] = (n0vec[0] / np.linalg.norm(np.array(currentDir)))
+            n0vec[1] = (n0vec[1] / np.linalg.norm(np.array(currentDir)))
+            n0vec[2] = (n0vec[2] / np.linalg.norm(np.array(currentDir)))
+            candPoint[7] = n0vec.copy()
             
             candChi2 = chiSquared(candPoint, dataset)
             
             if(candChi2 <= currentChi2 or np.random.rand(1)[0] < np.exp(currentChi2 - candChi2)):
+                #print(f"step: {100*i + j}")
                 currentPoint = candPoint
                 currentChi2 = candChi2
                 numberStepsIn100 = numberStepsIn100 + 1
@@ -84,6 +87,7 @@ def MCMC(dataset, totalPoints, initPoint, run = 0):
             stepSize = stepSize / 2
         elif(stepSize < .0001): # steps too small, but not getting too big either
             stepSize = stepSize * 2"""
+        #print(numberStepsIn100 / 100.0)
             
     pointsDict = {
         "O_m": oms,
@@ -106,8 +110,35 @@ def MCMC(dataset, totalPoints, initPoint, run = 0):
     
 def main():
     
-    randomPoints = RandomPoints()
-    startPoint = [.25, .04, .69, .01, .01, 0, .7, [1, 0, 0]]
+    dir1, dir2, dir3 = np.random.rand(3)
+    currentDir = [dir1, dir2, dir3]
+    n0vec = [dir1 / np.linalg.norm(np.array(currentDir)), dir2 / np.linalg.norm(np.array(currentDir)), dir3 / np.linalg.norm(np.array(currentDir))]
+    
+    O_m = np.random.normal(.3, .01, 1)[0]
+    while O_m < 0:
+        O_m = np.random.normal(.3, .01, 1)[0]
+        
+    O_r = np.random.normal(0, .01, 1)[0]
+    while O_r < 0:
+        O_r = np.random.normal(0, .01, 1)[0]
+        
+    O_B = np.random.normal(0, .01, 1)[0]
+    while O_B < 0:
+        O_B = np.random.normal(0, .01, 1)[0]
+        
+    O_L = np.random.normal(.7, .01, 1)[0] 
+    
+    b0 = np.random.normal(0, .01, 1)[0]
+    while b0 < -3 or b0 > 3:
+        b0 = np.random.normal(0, .01, 1)[0]
+        
+    #h between .6 and .8
+    h = .2 * np.random.rand(1)[0] + 0.6
+    
+    O_k = 1 - O_m - O_r - O_L - O_B - np.power(b0, 2)
+        
+    startPoint = [O_m, O_r, O_L, O_k, O_B, b0, h, n0vec]
+    
     print(startPoint)
     
     dataset = DataSet("SimulatedData.csv")

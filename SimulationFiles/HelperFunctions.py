0c4b27becc8d9949e7ab10e3a5a03607c513cb28
MichaelSeifert <mseifer1@conncoll.edu>
2024-09-17 15:06:26 -0400

    initial commit; helperfunctions broken

--- SimulationFiles/HelperFunctions.py
+++ SimulationFiles/HelperFunctions.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from ABvacmetric0 import ABvacmetric0
+import pde 
 
 def vectorfield(z, w, p):
     
@@ -29,19 +30,26 @@ def vectorfield(z, w, p):
     f = [-(1+z)/Q, (np.exp(-2*A + 2*B))/Q, (np.exp(-4*A - 2*B))/(Q * np.power(1 + z, 2))]
     
     return f
-    
 
-def helperfunctions(p, targetzvals=[]):
+def qfunction(cth, ABsolution):
+    
+    A, B, C, D = ABsolution(t)
+    Q = (((C + 2*D)*np.exp(-2*A - 4*B)*math.pow(cth,2))+
+         ((C-D)*np.exp(-2*A + 2*B)*(1 - math.pow(cth,2))))
+    
+    return Q
+
+
+def helperfunctions(ABsolution, targetzvals=[]):
     """
     Solves the ODE relating t, q, psi. 
 
     Arguments:
-        p :  vector of the parameters
-                  p = [ABsolution, theta_0]
+        ABsolution: ODE solution object from ABvacmetric0
         targetzvals : sorted array of desired z values for calculation of 
-        helper functions.  Default:  None
+        helper functions.  Default:  None.  NOT YET SUPPORTED
     """
-    
+
     if len(targetzvals) == 0:
         continuousOutput = True  
         maxz = 2
@@ -52,31 +60,78 @@ def helperfunctions(p, targetzvals=[]):
         maxz = targetzvals[-1]
         minz = 0
         # If specific z values requested, no continuous solution needed
-        
-    vectorizedInput = True; 
-    # defines whether or not the solver is handling a system or an individual ODE
+
+
+    grid = pde.CartesianGrid([[0,1]], 20, periodic=False)
+    tau = pde.ScalarField(grid)
+    q = pde.ScalarField(grid)
+    psi = pde.ScalarField(grid)
+    # The line below returns a "not enough values to unpack" error
+    A, B, C, D = ABsolution
     
-    zRange = [minz, maxz] # defines the range of z values to solve over
+    eq = PDE(
+        {
+            "tau": -(1+t)/qfunction(x,ABsolution),
+            "q": (np.exp(-2*A(t) + 2*B(t)))/qfunction(x,ABsolution),
+            "psi": np.exp(-4*A(t) - 2*B(t))/(qfunction(x,ABsolution)
+                                             * np.power(1 + t, 2)),
+            }
+        )
     
-    initVal = [0, 0, 0] # defines initial values for t, q, psi
+    print('So far so good!')
+
     
-    # calculate the solution. Returns a Bunch object with many fields.
-    solution = solve_ivp(vectorfield, zRange, initVal, 
-                         # method = 'RK45',
-                         dense_output = continuousOutput, 
-                         vectorized = vectorizedInput, 
-                         t_eval = targetzvals,
-                         args = (p,),
-                         atol = 1e-9
-                         )
-    
-    sol = solution.sol  # the continuous solution of the differential equation 
-                        # as instance of OdeSolution
-    funcvals = solution.y # Explicitly calculated function values
-    redshifts = solution.t # the redshifts that were considered.  If 
-                           # targetzvals was provided, this should be the same.
-    
-    return [sol, redshifts, funcvals]
+
+
+# =============================================================================
+# def oldhelperfunctions(p, targetzvals=[]):
+#     """
+#     Solves the ODE relating t, q, psi. 
+# 
+#     Arguments:
+#         p :  vector of the parameters
+#                   p = [ABsolution, theta_0]
+#         targetzvals : sorted array of desired z values for calculation of 
+#         helper functions.  Default:  None
+#     """
+#     
+#     if len(targetzvals) == 0:
+#         continuousOutput = True  
+#         maxz = 2
+#         minz = 0
+#         # If no specific z values requested, continuous solution returned
+#     else:
+#         continuousOutput = False
+#         maxz = targetzvals[-1]
+#         minz = 0
+#         # If specific z values requested, no continuous solution needed
+#         
+#     vectorizedInput = True; 
+#     # defines whether or not the solver is handling a system or an individual ODE
+#     
+#     zRange = [minz, maxz] # defines the range of z values to solve over
+#     
+#     initVal = [0, 0, 0] # defines initial values for t, q, psi
+#     
+#     # calculate the solution. Returns a Bunch object with many fields.
+#     solution = solve_ivp(vectorfield, zRange, initVal, 
+#                          # method = 'RK45',
+#                          dense_output = continuousOutput, 
+#                          vectorized = vectorizedInput, 
+#                          t_eval = targetzvals,
+#                          args = (p,),
+#                          atol = 1e-9
+#                          )
+#     
+#     sol = solution.sol  # the continuous solution of the differential equation 
+#                         # as instance of OdeSolution
+#     funcvals = solution.y # Explicitly calculated function values
+#     redshifts = solution.t # the redshifts that were considered.  If 
+#                            # targetzvals was provided, this should be the same.
+#     
+#     return [sol, redshifts, funcvals]
+# 
+# =============================================================================
 
 def main():    
     
@@ -90,15 +145,17 @@ def main():
     
     fig, ax = plt.subplots(3, 1, sharex = True)
     
-    cthvals=[0.01, 0.1, 0.2, 0.5, 1]
-    for cth in reversed(cthvals):
-        sol, redshifts, funcvals = helperfunctions([equation, cth])
-        redshifts = np.linspace(0.01,2.,100)
-        t, q, psi = sol(redshifts)
-        ax[0].plot(redshifts, t, label=cth)
-        ax[1].plot(redshifts, q)
-        ax[2].plot(redshifts, psi)
-        
+# =============================================================================
+#     cthvals=[0.01, 0.1, 0.2, 0.5, 1]
+#     for cth in reversed(cthvals):
+#         sol, redshifts, funcvals = helperfunctions([equation, cth])
+#         redshifts = np.linspace(0.01,2.,100)
+#         t, q, psi = sol(redshifts)
+#         ax[0].plot(redshifts, t, label=cth)
+#         ax[1].plot(redshifts, q)
+#         ax[2].plot(redshifts, psi)
+#         
+# =============================================================================
     ax[0].set_ylabel('tau_e')
     ax[1].set_ylabel('q_o')
     ax[2].set_ylabel('psi_o')
@@ -107,12 +164,12 @@ def main():
     fig.legend(loc='outside upper right')
     plt.show()
     
-    print(sol(.5))
+    # print(sol(.5))
     
-    sol, redshifts, funcvals = helperfunctions([equation, 0.5], [0, 0.5, 1.0, 1.5])
+    dummy = helperfunctions([equation, 0.5], [0, 0.5, 1.0, 1.5])
     
-    print(redshifts)
-    print(funcvals)
+    # print(redshifts)
+    # print(funcvals)
     
 if(__name__ == "__main__"):
     main()
